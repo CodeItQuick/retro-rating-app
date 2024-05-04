cc5f72e3d9047c235bc12e2afddc26c5d7d19e43
CodeItQuick <evanontario@hotmail.com>
2024-05-04 16:07:42 -0300

    works maybe

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import {useEffect, useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import {useHref, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 const uuidv4 = () => {
     const makeGuid = () => {
