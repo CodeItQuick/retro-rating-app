8b5a35050aa933addaa975489688f163cc450a90
CodeItQuick <evanontario@hotmail.com>
2024-05-04 10:14:22 -0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,9 @@ export default defineConfig({
   build: {
     // generate .vite/manifest.json in outDir
     manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/src/main.ts',
-    },
+    // rollupOptions: {
+    //   // overwrite default .html entry
+    //   // input: '/src/main.ts',
+    // },
   },
 })
